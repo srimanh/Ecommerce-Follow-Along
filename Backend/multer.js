0c9d798738dac7eae8d75b0fe04a3d9b1f37e8b0
unknown <srimandgl2004@gmail.com>
2025-01-27 17:07:47 +0530

    Completed a Milestone-4

---
+++ Backend/multer.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+// Configure multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Define your upload folder
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    // 1675276752705-752328184
+
+    // Define a unique filename
+    const filename = file.originalname.split('.')[0];
+
+    // image.png take only image
+    cb(null, filename + '-' + uniqueSuffix + '.png'); // Define
+    // image-1624234567-123456789.png.
+  },
+});
+
+// Initialize upload object
+exports.upload = multer({ storage: storage });
